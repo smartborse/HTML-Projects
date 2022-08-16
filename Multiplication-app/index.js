9dde7e29f269022af54ee4a7b94b2b29f7c3e660
pankaj <smartborse@gmail.com>
2022-08-16 18:38:55 +0530

    all projects are commited

---
+++ Multiplication-app/index.js
@@ -0,0 +1,7 @@
+const num1 = Math.ceil(Math.random()*10);
+
+const num2 = Math.ceil(Math.random()*10);
+
+const questionE1 = document.getElementById("question");
+
+questionE1.innerText = 'What is ${num1} multiply by ${num2}?';
